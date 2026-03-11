5904cf3f2024e273e699a40d6528ec87518a0625
asahel101837 <amartinez101837@ppsc.edu.mx>
2026-03-10 22:21:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ selectedFile = event.target.files[0];
 document.getElementById("status").innerText =
 "Foto capturada";
 });
+
+let lat;
+let lon;
+let precision;
+navigator.geolocation.getCurrentPosition((pos)=>{
+lat = pos.coords.latitude;
+lon = pos.coords.longitude;
+precision = pos.coords.accuracy;
+});
+async function generarHash(file){
+
+const buffer = await file.arrayBuffer();
+
+const hashBuffer =
+await crypto.subtle.digest("SHA-256", buffer);
+
+const hashArray =
+Array.from(new Uint8Array(hashBuffer));
+
+return hashArray
+.map(b => b.toString(16).padStart(2,"0"))
+.join("");
+
+}
